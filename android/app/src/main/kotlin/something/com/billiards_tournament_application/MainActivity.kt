ff1f91e5da764ae1e7a84b9581192cff11a12e19
Augustine <morrisaugustine2@gmail.com>
2025-05-10 11:08:22 -0500

    Starting Flutter App

---
+++ android/app/src/main/kotlin/something/com/billiards_tournament_application/MainActivity.kt
@@ -0,0 +1,5 @@
+package something.com.billiards_tournament_application
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  home: Scaffold(
+    appBar: AppBar(
+      title: Image.asset('./8-9-10.png', fit: BoxFit.cover),
+    ),
+  ),
+));
